b5d0874cc2d6accd1678f9c625810e37d2f65a8a
Heather Diamond <baishenkokv@gmail.com>
2025-03-04 19:58:03 +0300

    update with case

--- main.py
+++ main.py
@@ -1,37 +1,24 @@
 try:
     num1 = int(input("\nВведите первое число: "))
     num2 = int(input("Введите второе число: "))
-    result = num1 + num2
-except ValueError:
-    print("Ошибка: введено не число!")
-else:
-    print(f"Результат сложения: {result}")
-
-try:
-    num3 = int(input("\nВведите первое число: "))
-    num4 = int(input("Введите второе число: "))
-    result = num3 - num4
-except ValueError:
-    print("Ошибка: введено не число!")
-else:
-    print(f"Результат вычитания: {result}")
-
-try:
-    num5 = int(input("\nВведите первое число: "))
-    num6 = int(input("Введите второе число: "))
-    result = num5 * num6
-except ValueError:
-    print("Ошибка: введено не число!")
-else:
-    print(f"Результат умножения: {result}")
-
-try:
-    num7 = int(input("\nВведите первое число: "))
-    num8 = int(input("Введите второе число: "))
-    result = num7 / num8
+    print(
+        "Выберите номер 1, для операции сложения; 2, для операции вычитания; \n               3, для операции умножения; 4, для операции деления;")
+    num3 = int(input("Введите порядковый номер операции: "))
+    match num3:
+        case 1:
+            result = num1 + num2
+        case 2:
+            result = num1 - num2
+        case 3:
+            result = num1 * num2
+        case 4:
+            result = num1 / num2
+        case _:
+            result = "-"
+            print("Указанный номер не соответсвует списку операций :(")
 except ValueError:
     print("Ошибка: введено не число!")
 except ZeroDivisionError:
     print("Ошибка: Деление на ноль!")
 else:
-    print(f"Результа деленият: {result}")
+    print(f"Результат операции: {result}")
