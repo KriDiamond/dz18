c5e869e7f2cf592c152df743f1a77eaeffaa170f
Heather Diamond <baishenkokv@gmail.com>
2025-03-15 12:32:46 +0300

    add space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def max_number(a, b):
-    if b>a: a=b
+    if b > a: a = b
     return a
 
 
@@ -7,7 +7,7 @@ def empty_function():
     pass
 
 
-def even_numbers( n ):
+def even_numbers(n):
     for i in range (0, n + 1, 2):
         yield i
 
@@ -19,7 +19,7 @@ def test_max_number():
     assert max_number(5, 5) == 5, "Ошибка: максимум из (5, 5) должен быть равен 5"
 
 
-print(max_number(3,5))
+print(max_number(3, 5))
 for even in even_numbers(6):
     print(even)
 test_max_number()
