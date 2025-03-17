a62d39f127006be813dca80ba8e62724b3eb266d
Heather Diamond <baishenkokv@gmail.com>
2025-03-17 13:16:14 +0300

    add edit 1

--- main.py
+++ main.py
@@ -4,3 +4,27 @@ def check_oportunity_to_vote(person_age, person_citizenship, person_disqualifica
     else:
         result = False
     return result
+
+
+try:
+    age = int(input("Введите ваш возраст: "))
+    answer = (input("Имеется ли у вас гражданство страны (да/нет): ")).lower()
+    if answer == 'да':
+        is_citizen = True
+    else:
+        is_citizen = False
+    answer = (input("Имеется ли у вас дисквалификация, например из-за судимости (да/нет): ")).lower()
+    if answer == 'да':
+        is_disqualificated = True
+    else:
+        is_disqualificated = False
+    if check_oportunity_to_vote(age, is_citizen, is_disqualificated):
+        result = 'Вы допущены до голосования. Поздравляем!'
+    else:
+        result = 'Вы не допущены до голосования.'
+except ValueError:
+    print("Ошибка: Не верный формат данных!")
+else:
+    print(f"Результат: {result}")
+finally:
+    print('Программа завершила работу.')
