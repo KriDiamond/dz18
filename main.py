ceecf5bf92f9c98fbb083c75ebef66e5dde05757
Heather Diamond <baishenkokv@gmail.com>
2025-03-17 21:21:53 +0300

    add edit 2

--- main.py
+++ main.py
@@ -1,30 +1,18 @@
 def check_oportunity_to_vote(person_age, person_citizenship, person_disqualification):
-    if person_age >= 18 and person_citizenship == True and person_disqualification == False:
-        result = True
+    if person_age >= 18 and person_citizenship == 'да' and person_disqualification == 'нет':
+        result = 'Вы допущены до голосования. Поздравляем!'
     else:
-        result = False
+        result = 'Вы не допущены до голосования.'
     return result
 
 
 try:
     age = int(input("Введите ваш возраст: "))
-    answer = (input("Имеется ли у вас гражданство страны (да/нет): ")).lower()
-    if answer == 'да':
-        is_citizen = True
-    else:
-        is_citizen = False
-    answer = (input("Имеется ли у вас дисквалификация, например из-за судимости (да/нет): ")).lower()
-    if answer == 'да':
-        is_disqualificated = True
-    else:
-        is_disqualificated = False
-    if check_oportunity_to_vote(age, is_citizen, is_disqualificated):
-        result = 'Вы допущены до голосования. Поздравляем!'
-    else:
-        result = 'Вы не допущены до голосования.'
+    is_citizen = (input("Имеется ли у вас гражданство страны (да/нет): ")).lower()
+    is_disqualificated = (input("Имеется ли у вас дисквалификация, например из-за судимости (да/нет): ")).lower()
 except ValueError:
     print("Ошибка: Не верный формат данных!")
 else:
-    print(f"Результат: {result}")
+    print(f"Результат: {check_oportunity_to_vote(age, is_citizen, is_disqualificated)}")
 finally:
     print('Программа завершила работу.')
